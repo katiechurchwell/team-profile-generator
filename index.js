4d19572c84eae86e88277ddd92d1d495d173bc2a
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-16 18:11:51 -0800

    inquirer in index only

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const Manger = require("./lib/manager");
-const inquirer = require('inquirer');
-
+// const Manger = require("./lib/manager");
+const inquirer = require("inquirer");
 
 console.log("Welcome to the Team Profile Generator");
 
+const promptUser = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Employee name? (required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Employee id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Email?",
+    },
+    {
+      type: "input",
+      name: "phone",
+      message: "Office Number?",
+    },
+  ]);
+};
+
+promptUser()
 
 /* team manager */
-
 
 /* then engineer or intern option menu */
 
