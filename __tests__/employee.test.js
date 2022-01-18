8ba259552ffe75658251e28612505e53a90bfff9
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-17 16:52:17 -0800

    employee test

---
+++ __tests__/employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require("../lib/employee.js");
+
+test("Employee name working?", () => {
+  const employee = new Employee("Katie", "id", "email");
+  expect(employee.name).toEqual("Katie");
+});
+
+test("Employee id working?", () => {
+  const employee = new Employee("name", "id", "email");
+  expect(employee.id).toEqual("id");
+});
+
+test("Employee email working?", () => {
+  const employee = new Employee("name", "id", "email");
+  expect(employee.email).toEqual("email");
+});
+
+test("getName() working?", () => {
+    const employee = new Employee("name", "id", "email");
+    expect(employee.getName()).toEqual("name");
+  });
+
+  test("getId() working?", () => {
+    const employee = new Employee("name", "id", "email");
+    expect(employee.getId()).toEqual("id");
+  });
+
+  test("getEmail() working?", () => {
+    const employee = new Employee("name", "id", "email");
+    expect(employee.getEmail()).toEqual("email");
+  });
+
+  test("getRole() working?", () => {
+    const employee = new Employee("name", "id", "email");
+    expect(employee.getRole()).toEqual("Employee");
+  });
